efdb5553c7c12cee2c1a87dfb39b29af4e093d5e
Cristian Bidea <cristian.bidea@uipath.com>
2026-01-09 09:51:27 +0200

    Implemented renderCustomProgram

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: 'module',
+    },
+  },
+};
